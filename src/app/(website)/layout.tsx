b38d4a0efd5b048f541084c49069abec0961a7f5
Punyo Sunya <sunya22839@example.com>
2025-05-22 12:11:59 +0530

    added the nav bar page

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Layout = ({ children }: Props) => {
     return (
-        <div className="flex flex-col py-10 px-10 xl:px-0 container">
+        <div className="flex flex-col py-10 px-10 xl:px-5 container">
             <LandingPageNavBar />
             {children}
         </div>
